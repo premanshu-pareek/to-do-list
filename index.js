fa9d723f2cd8ce09458cff46f81b7616ed8434e9
Premanshu Pareek <premanshupareek@Premanshus-MacBook-Air.local>
2024-11-17 13:48:33 +0530

    TDL-02: Adds LocalStorage Suport

--- index.js
+++ index.js
@@ -8,13 +8,32 @@ function addTask() {
     const li = document.createElement("LI");
     li.textContent = searchBar.value;
     toDoContainer.appendChild(li);
+
+    const span = document.createElement("SPAN");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
 
   searchBar.value = "";
+  saveData();
 }
 
 toDoContainer.addEventListener("click", function(e) {
-  if(e.target.tagName == "LI") {
+  if(e.target.tagName === "LI") {
     e.target.classList.toggle("checked")
+    saveData();
+  } else if(e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
   }
 })
+
+function saveData() {
+  localStorage.setItem("data", toDoContainer.innerHTML);
+}
+
+function showTask() {
+  toDoContainer.innerHTML = localStorage.getItem("data")
+}
+
+showTask();
